eac97ec58eb0480a70e1d6183fbd1ef8b53c34d4
fadyallabbani <fady.abood@gmail.com>
2022-01-16 14:19:08 -0500

    day39 Recap

--- src/day39_Recap/cydeoTask/Tester.java
+++ src/day39_Recap/cydeoTask/Tester.java
@@ -9,4 +9,10 @@ public class Tester extends Employee {
     public void createTicket(){
         System.out.println(getJobTitle()+" "+ getName()+"is creating ticket");
     }
+
+    @Override
+    public void work() {
+        System.out.println(getJobTitle()+" "+getName()+"is testing the application ");
+    }
+
 }
